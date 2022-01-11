d38569689b4e759a05a39d67ba80c24b82cbf02d
honeykikiki <tjdwls75666@naver.com>
2022-01-11 22:20:37 +0900

    usereducer, createContext 타입적용

--- lecture/MineTable.tsx
+++ lecture/MineTable.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
+import { TableContext } from './MineSearch';
+import { useContext } from 'react';
+import MineTr from './MineTr';
 
-const MineTable = () => {
-  return <></>;
+const Table = () => {
+  const { tableData } = useContext(TableContext);
+  return (
+    <table>
+      <tbody>
+        {Array(tableData.length)
+          .fill(null)
+          .map((tr, i) => (
+            <MineTr key={i} rowIndex={i} />
+          ))}
+      </tbody>
+    </table>
+  );
 };
 
-export default MineTable;
+export default Table;
